98eed6d63971b5e4cf1eef38f6b5fb7b92961bea
jorgeantonio21 <matroid@outlook.com>
2023-08-24 12:25:29 +0100

    leaf proof struct

--- src/leaf_proof.rs
+++ src/leaf_proof.rs
@@ -1,1 +1,18 @@
-pub struct LeafProof {}
+use plonky2::{
+    field::extension::Extendable,
+    hash::hash_types::RichField,
+    plonk::config::{AlgebraicHasher, GenericConfig},
+};
+
+use crate::proof_data::ProofData;
+
+pub struct LeafProof<C, F, H, const D: usize>
+where
+    H: AlgebraicHasher<F>,
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F>,
+{
+    input_hash: H,
+    circuit_hash: H,
+    proof_data: ProofData<F, C, D>,
+}
